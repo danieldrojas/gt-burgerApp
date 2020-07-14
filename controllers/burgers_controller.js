86f5147eabd24b21c0d8d782b153248d2a50ad53
Daniel Rojas <daniel.drojas@outlook.com>
2020-07-13 20:20:13 -0400

    created the controllers folder and burgers_controllers file

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const burger = require("../models/burger.js");
+
+const router = express.Router();
+
+
+
+// ????? 4. Create the`router` for the app, and export the`router` at the end of your file.
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
