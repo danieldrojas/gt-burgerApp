0b43ef98ae7874158bfaf202cf5338e916e4aa4a
Daniel Rojas <daniel.drojas@outlook.com>
2020-07-14 06:41:08 -0400

    created burger obj with property methods all, insert, update

--- models/burger.js
+++ models/burger.js
@@ -3,15 +3,34 @@ const orm = require("../config/orm.js");
 
 
 
+let burger = {
+
+    all: function (cb) {
+
+        orm.selectAll("burgers", function (res) {
+            cb(res)
+        })        
+    },
+
+    insert: function (colName, value, cb) {
+        orm.insertOne("burgers", colName, value, function (res) {
+            cb(res);
+        })
+    },
+    update: function (colName, value, condition, cb) {
+        orm.updateOne("burgers", colName, value, condition, function (res) {
+            cb(res);
+        } )
+    }
+
+
+}
 
 
 
-orm.selectAll("burgers")
 
 
 
 
 
-
-
-// module.exports = 
+module.exports = burger;
