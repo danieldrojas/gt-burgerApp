f5040a505cc54ed0ad6f401a3658c8ca6fe516ad
Daniel Rojas <daniel.drojas@outlook.com>
2020-07-13 20:15:24 -0400

    created config folder with connection and orm files

---
+++ config/orm.js
@@ -0,0 +1,48 @@
+const  connection  = require("../config/connection.js")
+
+//select method
+var orm = {
+    selectAll: function (tableName, cb) {
+        const queryString = "SELECT * FROM " + tableName + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+
+            cb(result);
+        
+        });
+
+    },
+    insertOne: function (tableName, colName, value, cb) {
+        let queryString = "INSERT INTO "; queryString += tableName;
+        queryString += " SET ";
+        queryString += colName;
+        queryString += " = ";
+        queryString += value
+
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    updateOne: function (tableName, colName, value, condition,  cb) {
+        let queryString = "UPDATE "; queryString += tableName;
+        queryString += " SET ";
+        queryString += colName;
+        queryString += " = ";
+        queryString +=  value;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    }
+};
+
+
+        
+
+
+
+module.exports = orm;
